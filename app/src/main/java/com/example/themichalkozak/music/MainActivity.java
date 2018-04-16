3bd3f736e5dd5f13cfcf98cc1f61695f6e3b1e80
seeminglySafe <seeminglysafe@gmail.com>
2018-04-16 21:15:37 +0200

    problem with listview

--- app/src/main/java/com/example/themichalkozak/music/MainActivity.java
+++ app/src/main/java/com/example/themichalkozak/music/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.themichalkozak.music;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -23,45 +25,39 @@ public class MainActivity extends AppCompatActivity {
     public static final ArrayList<Album> albums = new ArrayList<Album>();
 
     public static final ArrayList<Track> tracks = new ArrayList<Track>();
+
+    int isCreated = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Uri uriArtist = Uri.parse("android.resource://com.example.themichalkozak.music/" + R.drawable.artisticon);
         Uri uriAlbum = Uri.parse("android.resource://com.example.themichalkozak.music/" + R.drawable.albumicon);
         String ostrArtistDrawable = uriArtist.toString();
-        String albumDrawable = uriArtist.toString();
-//
-//            addArtist("O.S.T.R.",ostrArtistDrawable);
-//            addArtist("Małpa",ostrArtistDrawable);
-//            addArtist("Edd Sheeran",ostrArtistDrawable);
-//
-//            addAlbum("Małpa","Pozwól mi nie mówić nic",getDrawable(R.drawable.albumicon));
-//            addAlbum("O.S.T.R.","Tabasko",getDrawable(R.drawable.albumicon));
-//            addAlbum("Edd Sheeran","Divide",getDrawable(R.drawable.albumicon));
+        String albumDrawable = uriAlbum.toString();
 
-            String ostr [] = getResources().getStringArray(R.array.ostr);
-            String edSheeran [] = getResources().getStringArray(R.array.ed_sheeran_array);
-            String malpa [] = getResources().getStringArray(R.array.malpa);
-//
-        addArtist(ostr[0],ostrArtistDrawable);
-        addArtist(malpa[0],ostrArtistDrawable);
-        addArtist(edSheeran[0],ostrArtistDrawable);
+            String ostr[] = getResources().getStringArray(R.array.ostr);
+            String edSheeran[] = getResources().getStringArray(R.array.ed_sheeran_array);
+            String malpa[] = getResources().getStringArray(R.array.malpa);
 
-        addAlbum(edSheeran[0],edSheeran[1],albumDrawable);
-        addAlbum(malpa[0],malpa[1],albumDrawable);
-        addAlbum(ostr[0],ostr[1],albumDrawable);
+            addArtist(ostr[0], ostrArtistDrawable);
+            addArtist(malpa[0], ostrArtistDrawable);
+            addArtist(edSheeran[0], ostrArtistDrawable);
+
+            addAlbum(edSheeran[0], edSheeran[1], albumDrawable);
+            addAlbum(malpa[0], malpa[1], albumDrawable);
+            addAlbum(ostr[0], ostr[1], albumDrawable);
 
 
+            for (int i = 2; i < ostr.length; i++) {
+                addTrack(edSheeran[0], edSheeran[1], edSheeran[i]);
+                addTrack(malpa[0], malpa[1], malpa[i]);
+                addTrack(ostr[0], ostr[1], ostr[i]);
 
-        for(int i=2;i<ostr.length;i++){
-            addTrack(edSheeran[0],edSheeran[1],edSheeran[i]);
-            addTrack(malpa[0],malpa[1],malpa[i]);
-            addTrack(ostr[0],ostr[1],ostr[i]);
-
-        }
-
+            }
 
         createAlbumList();
         createTrackList();
@@ -107,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addArtist(String artistName, String drawable){
+        isCreated = 1;
         artistArrayList.add(new Artist(artistName,drawable));
     }
     public void addAlbum(String artistName, String album, String drawable){
@@ -152,4 +149,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+
 }
